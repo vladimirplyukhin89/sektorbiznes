41431a1266333f49e17cde96de59a7cca8edebca
Vladimir Plyukhin <vladimirplyukhin89@gmail.com>
2023-07-26 06:52:30 -1100

    Changed semantic HTML in SortTable

--- src/components/components/Posts/TablePosts/SortPosts/SortPosts.tsx
+++ src/components/components/Posts/TablePosts/SortPosts/SortPosts.tsx
@@ -1,102 +1,51 @@
 import React from "react";
 
-import arrowIcon from "./image/arrow.svg";
-
 interface IProps {
 	setSortOrder: React.Dispatch<React.SetStateAction<"asc" | "desc">>;
 	setSortType: React.Dispatch<React.SetStateAction<"id" | "title">>;
 }
 
 export const SortPosts: React.FC<IProps> = ({ setSortOrder, setSortType }) => {
-	const [idAsc, setIdAsc] = React.useState<"По возрастанию" | "По убыванию">(
-		"По возрастанию"
-	);
-	const [titleAsc, setTitleAsc] = React.useState<
-		"По возрастанию" | "По убыванию"
-	>("По возрастанию");
-
 	const handleSortedPosts = (type: string) => {
 		if (type === "id") {
 			setSortOrder((prevSortOrder) =>
 				prevSortOrder === "asc" ? "desc" : "asc"
 			);
 			setSortType("id");
-			setIdAsc(idAsc === "По возрастанию" ? "По убыванию" : "По возрастанию");
 		} else if (type === "title") {
 			setSortOrder((prevSortOrder) =>
 				prevSortOrder === "asc" ? "desc" : "asc"
 			);
 			setSortType("title");
-			setTitleAsc(
-				titleAsc === "По возрастанию" ? "По убыванию" : "По возрастанию"
-			);
 		}
 	};
 
+	const renderSortButton = (label: string, sortType: "id" | "title") => {
+		return (
+			<th className="flex justify-center w-[56px] lg:w-[112px]">
+				<span
+					onClick={() => handleSortedPosts(sortType)}
+					className="cursor-pointer"
+				>
+					<span className="me-[18px] lg:me-[39px]">{label}</span>
+				</span>
+			</th>
+		);
+	};
+
 	return (
-		<div className="grid grid-cols-[56px_216px_413px] lg:grid-cols-[112px_480px_433px] bg-[#474955] text-white pt-[19px] pb-[16px]">
-			<div className="flex justify-center w-[56px] lg:w-[112px]">
-				<p className="me-[18px] lg:me-[39px]">ID</p>
-				<div className="relative group">
-					<button>
-						<img
-							src={arrowIcon}
-							alt="click me"
-							className="group-hover:scale-125 group-hover:animate-pulse"
-						/>
-					</button>
-					<div className="bg-[#7EBC3C]/[0.9] rounded-sm p-1 w-[130px] text-center absolute top-30% right-0 left-[-50px] md:left-[-60px] opacity-0 group-hover:opacity-100">
-						<button
-							onClick={() => handleSortedPosts("id")}
-							className="text-sm hover:text-[#474955] hover:font-bold"
-						>
-							{idAsc}
-						</button>
-					</div>
-				</div>
-			</div>
+		<thead className="grid grid-cols-[56px_216px_413px] lg:grid-cols-[112px_480px_433px] bg-[#474955] text-white pt-[19px] pb-[16px]">
+			<tr className="flex justify-center w-[56px] lg:w-[112px]">
+				{renderSortButton("ID", "id")}
+			</tr>
 
-			<div className="flex justify-center w-[216px] lg:w-[480px]">
-				<p className="me-[18px] md:me-[34px]">Заголовок</p>
-				<div className="relative group">
-					<button>
-						<img
-							src={arrowIcon}
-							alt="click me"
-							className="group-hover:scale-125 group-hover:animate-pulse"
-						/>
-					</button>
-					<div className="bg-[#7EBC3C]/[0.9] rounded-sm p-1 w-[130px] text-center absolute top-30% right-0 left-[-50px] md:left-[-60px] opacity-0 group-hover:opacity-100">
-						<button
-							onClick={() => handleSortedPosts("title")}
-							className="text-sm hover:text-[#474955] hover:font-bold"
-						>
-							{titleAsc}
-						</button>
-					</div>
-				</div>
-			</div>
+			<tr className="flex justify-center w-[216px] lg:w-[480px]">
+				{renderSortButton("Заголовок", "title")}
+			</tr>
 
-			<div className="flex justify-start ps-[30px] md:ps-[60px] lg:ps-[121px] w-[413px] lg:w-[433px]">
-				<p className="me-[18px] md:me-[25px]">Описание</p>
-				<div className="relative group">
-					<button>
-						<img
-							src={arrowIcon}
-							alt="click me"
-							className="group-hover:scale-125 group-hover:animate-pulse"
-						/>
-					</button>
-					<div className="bg-[#7EBC3C]/[0.9] rounded-sm p-1 w-[130px] text-center absolute top-30% right-0 left-[-50px] md:left-[-60px] opacity-0 group-hover:opacity-100">
-						<button
-							onClick={() => handleSortedPosts("title")}
-							className="text-sm hover:text-[#474955] hover:font-bold"
-						>
-							{titleAsc}
-						</button>
-					</div>
-				</div>
-			</div>
-		</div>
+			<tr className="flex justify-start ps-[30px] md:ps-[60px] lg:ps-[121px] w-[413px] lg:w-[433px]">
+				{renderSortButton("Описание", "title")}
+			</tr>
+		</thead>
 	);
 };
